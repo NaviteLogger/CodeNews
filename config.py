ae83c8446e30f7ae17d81fe510973442537da42c
NaviteLogger <kacprzakmarek92@gmail.com>
2023-09-19 00:38:02 +0200

    Added the sample MySQL configuration to config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Database configuration
+DB_CONFIG = {
+    "host": "your_database_host",
+    "user": "your_database_user",
+    "password": "your_database_password",
+    "database": "your_database_name",
+}
