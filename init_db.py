ce26e4a426ff49b3712e662f43b537c0acd64c7e
NaviteLogger <kacprzakmarek92@gmail.com>
2023-09-21 19:00:11 +0200

    Added a try/catch structure to the init_db.py script to catch any potential error when creating the database tables

--- init_db.py
+++ init_db.py
@@ -7,33 +7,37 @@ connection = mysql.connector.connect(**DB_CONFIG)
 # Create a cursor object
 cursor = connection.cursor()
 
-# Create the 'news_articles' table
-create_news_articles_table_query = """
-CREATE TABLE IF NOT EXISTS news_articles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    content TEXT,
-    source VARCHAR(255) NOT NULL,
-    date_published DATE
-);
-"""
+try:
+    # Create the 'news_articles' table
+    create_news_articles_table_query = """
+    CREATE TABLE IF NOT EXISTS news_articles (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        content TEXT,
+        source VARCHAR(255) NOT NULL,
+        date_published DATE
+    );
+    """
 
-# Execute the SQL statements to create the table
-cursor.execute(create_news_articles_table_query)
+    # Execute the SQL statements to create the table
+    cursor.execute(create_news_articles_table_query)
 
-# Create the 'scrape_sources_urls' table
-create_scrape_sources_urls_table_query = """
-CREATE TABLE IF NOT EXISTS scrape_sources_urls (
+    # Create the 'scrape_sources_urls' table
+    create_scrape_sources_urls_table_query = """
+    CREATE TABLE IF NOT EXISTS scrape_sources_urls (
     id INT AUTO_INCREMENT PRIMARY KEY,
     source_name VARCHAR(255) NOT NULL,
     source_url VARCHAR(255) NOT NULL
-);
-"""
+    );
+    """
 
-# Execute the SQL statements to create the table
-cursor.execute(create_scrape_sources_urls_table_query)
+    # Execute the SQL statements to create the table
+    cursor.execute(create_scrape_sources_urls_table_query)
 
-# Commit changes and close the cursor and connection
+except Exception as e:
+    print("Error occurred while initializing the database schema: ", e)
+
+# Commit changes, close the cursor and connection
 connection.commit()
 cursor.close()
 connection.close()
