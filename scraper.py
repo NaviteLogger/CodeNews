f62b1e760e5a06d91d0d5805fbec944561166f2b
NaviteLogger <kacprzakmarek92@gmail.com>
2023-09-21 12:57:15 +0200

    Added the correct code formatting

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import mysql.connector
 from config import DB_CONFIG
 
+
 # This function will scrape and store news articles from the specified URL
 def scrape_and_store_news():
     # Create a database connection
@@ -11,36 +12,38 @@ def scrape_and_store_news():
     # Create a cursor - a middleware between the database and the application
     cursor = connection.cursor()
 
-    #Retrieve the source name and URL from the database
+    # Retrieve the source name and URL from the database
     cursor.execute("SELECT source_name, source_url FROM scrape_sources_urls")
     sources = cursor.fetchall()
 
     for source_name, source_url in sources:
         try:
-            #Send a HTTP GET request to the URL
+            # Send a HTTP GET request to the URL
             response = requests.get(source_url)
 
-            #Check if the request was successful
-            if response.status == '200':
-
-                #Parse the response using BeautifulSoup
-                soup = BeautifulSoup(response.text, 'html.parser')
+            # Check if the request was successful
+            if response.status == "200":
+                # Parse the response using BeautifulSoup
+                soup = BeautifulSoup(response.text, "html.parser")
 
                 # Extract article information (customize as needed)
-                title = soup.find('h1', class_='article-title').text
-                author = soup.find('span', class_='author-name').text
-                date_published = soup.find('span', class_='date').text
-                content = soup.find('div', class_='article-body').text
+                title = soup.find("h1", class_="article-title").text
+                author = soup.find("span", class_="author-name").text
+                date_published = soup.find("span", class_="date").text
+                content = soup.find("div", class_="article-body").text
                 source = source_url
 
                 # Insert the article into the database
-                cursor.execute("INSERT INTO news_articles (title, author, date_published, content, source) VALUES (%s, %s, %s, %s, %s)", (title, author, date_published, content, source))
+                cursor.execute(
+                    "INSERT INTO news_articles (title, author, date_published, content, source) VALUES (%s, %s, %s, %s, %s)",
+                    (title, author, date_published, content, source),
+                )
 
                 # Commit the changes to the database
                 connection.commit()
 
                 # Print the article information
-                print(f"Error scraping and storing data from {source_name}: {str(e)}")                
+                print(f"Error scraping and storing data from {source_name}: {str(e)}")
 
         except Exception as e:
             print("Error occurred while scraping: ", e)
@@ -48,8 +51,9 @@ def scrape_and_store_news():
     # Close the cursor
     cursor.close()
 
-    #Close the connection
+    # Close the connection
     connection.close()
+
 
 if __name__ == "__main__":
     scrape_and_store_news()
