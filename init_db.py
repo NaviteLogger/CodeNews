a41a98a58b7fa255c8df3deb95c3849e6787dbdc
NaviteLogger <kacprzakmarek92@gmail.com>
2023-09-21 18:57:16 +0200

    Added the initialization of two tables to the init_db.py script

--- init_db.py
+++ init_db.py
@@ -7,19 +7,31 @@ connection = mysql.connector.connect(**DB_CONFIG)
 # Create a cursor object
 cursor = connection.cursor()
 
-# Define SQL statements to create tables (customize these based on your project needs)
-create_table_query = """
+# Create the 'news_articles' table
+create_news_articles_table_query = """
 CREATE TABLE IF NOT EXISTS news_articles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT,
-    source VARCHAR(255),
+    source VARCHAR(255) NOT NULL,
     date_published DATE
 );
 """
 
-# Execute the SQL statements
-cursor.execute(create_table_query)
+# Execute the SQL statements to create the table
+cursor.execute(create_news_articles_table_query)
+
+# Create the 'scrape_sources_urls' table
+create_scrape_sources_urls_table_query = """
+CREATE TABLE IF NOT EXISTS scrape_sources_urls (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    source_name VARCHAR(255) NOT NULL,
+    source_url VARCHAR(255) NOT NULL
+);
+"""
+
+# Execute the SQL statements to create the table
+cursor.execute(create_scrape_sources_urls_table_query)
 
 # Commit changes and close the cursor and connection
 connection.commit()
